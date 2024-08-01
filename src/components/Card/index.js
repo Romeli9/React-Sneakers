95618db2e3b50ee802eb9fbbb52c3e169c087b0d
Romeli <butus.5@outlook.com>
2024-08-01 19:15:38 +0700

    components added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,19 @@
-function Card() {
+import styles from './Card.module.scss';
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="card__favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked"></img>
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt=""></img>
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt=""></img>
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена: </span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button type="button" className="button">
+        <button onClick={() => alert(123)} type="button" className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
