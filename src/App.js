1e647ca7779195355968b857d9e5e9090788ed98
Romeli <butus.5@outlook.com>
2024-08-05 15:08:16 +0700

    fix path

--- src/App.js
+++ src/App.js
@@ -147,7 +147,7 @@ function App() {
         />
         <Routes>
           <Route
-            path=""
+            path="/react-sneakers"
             element={
               <Home
                 items={items}
@@ -161,8 +161,8 @@ function App() {
             }
             exact
           />
-          <Route path="favorites" exact element={<Favorites />}></Route>
-          <Route path="orders" exact element={<Orders />}></Route>
+          <Route path="/react-sneakers/favorites" exact element={<Favorites />}></Route>
+          <Route path="/react-sneakers/orders" exact element={<Orders />}></Route>
         </Routes>
       </div>
     </AppContext.Provider>
